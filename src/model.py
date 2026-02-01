d4b15512f773c21338537b1c89aae60af044a9a5
PRATIK PAL <112645246+palpratik56@users.noreply.github.com>
2026-02-01 20:14:34 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,84 +1,92 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from blocks.py import (SEBlock, DoubleConv, Down, AttentionGate, UpConv, PAM_Module, PositionEmbeddingLearned, ScaledDotProductAttention)
+from blocks.py import (DoubleConv, Down, OutConv, UpConv, PAM_Module, ASIB, FAAB, TAAC )
 
-class MasetNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
-        super(MasetNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
-        
+class MASETNet(nn.Module):
+    def __init__(self, n_channels=3, n_classes=1, bilinear=True, use_pam=True, 
+                 use_asib=True, use_taac=True, use_faab=True):
+        super().__init__()
+
+        self.use_asib = use_asib
+        self.use_taac = use_taac
+        self.use_faab = use_faab
+        self.use_pam = use_pam
+
         self.inc = DoubleConv(n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
-        
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = UpConv(1024, 512 // factor, bilinear)
-        self.up2 = UpConv(1024, 256 // factor, bilinear)
-        self.up3 = UpConv(512, 128 // factor, bilinear)
-        self.up4 = UpConv(256, 64, bilinear)
+        self.down4 = Down(512, 512)
 
-        self.outc = nn.Conv2d(128, n_classes, kernel_size=1)
-        '''position encoding'''
-        self.pos = PositionEmbeddingLearned(512 // factor)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
 
-        '''spatial attention mechanism'''
-        self.pam = PAM_Module(512)
+        self.outc = OutConv(128, n_classes)
 
-        '''self-attention mechanism'''
-        self.sdpa = ScaledDotProductAttention(512)
+        self.pam  = PAM_Module(512)
+        # self.sdpa = ScaledDotProductAttention(512)
+        self.faab = FAAB(512)
+
+        self.asib1 = ASIB(256, 512, 256)
+        self.asib2 = ASIB(128, 256, 128)
+        self.asib3 = ASIB(64, 128, 64)
+
+        self.taac1 = TAAC(x_ch=128, task_ch=256)
+        self.taac2 = TAAC(x_ch=64,  task_ch=128)
+        self.taac3 = TAAC(x_ch=64,  task_ch=64)
+
 
     def forward(self, x):
+        feature_maps = {}
+
+         # -------- Encoder --------
         x1 = self.inc(x)
-        '''Encoder'''
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-
-        '''Bottle neck'''
-        '''Setting 1'''
-        x5_pam = self.pam(x5)
-
-        '''Setting 2'''
-        x5_pos = self.pos(x5)
-        x5 = x5 + x5_pos
-
-        '''Setting 3'''
-        x5_sdpa = self.sdpa(x5)
-        x5 = x5_sdpa + x5_pam
-
-        # Decoder path with feature map capture
-        feature_maps = {}
-
-        '''Decoder'''
-        x6 = self.up1(x5, x4)
-        feature_maps['d1'] = x6
-        x5_scale = F.interpolate(x5, size=x6.shape[2:], mode='bilinear', align_corners=True)
-        x6_cat = torch.cat((x5_scale, x6), 1)
-
-        x7 = self.up2(x6_cat, x3)
-        feature_maps['d2'] = x7
-        x6_scale = F.interpolate(x6, size=x7.shape[2:], mode='bilinear', align_corners=True)
-        x7_cat = torch.cat((x6_scale, x7), 1)
-
-        x8 = self.up3(x7_cat, x2)
-        feature_maps['d3'] = x8
-        x7_scale = F.interpolate(x7, size=x8.shape[2:], mode='bilinear', align_corners=True)
-        x8_cat = torch.cat((x7_scale, x8), 1)
-
-        x9 = self.up4(x8_cat, x1)
-        feature_maps['d4'] = x9
-        x8_scale = F.interpolate(x8, size=x9.shape[2:], mode='bilinear', align_corners=True)
-        x9 = torch.cat((x8_scale, x9), 1)
-
-        logits = self.outc(x9)
-        feature_maps['output'] = logits
-        return logits, feature_maps
+    
+        # -------- Bottleneck --------
+        if self.use_pam:
+            x5 = self.pam(x5)
+        if self.use_faab:
+            x5 = self.faab(x5)
+    
+        # -------- Decoder --------
+        d1 = self.up1(x5, x4)
+        if self.use_asib:
+            d1 = self.asib1(d1, x4)
+        feature_maps['dec1'] = d1
+    
+        d2 = self.up2(d1, x3)
+        if self.use_asib:
+            d2 = self.asib2(d2, x3)
+    
+        if self.use_taac:
+            d2 = self.taac1(d2, d1)
+        feature_maps['dec2'] = d2
+    
+        d3 = self.up3(d2, x2)
+        if self.use_asib:
+            d3 = self.asib3(d3, x2)
+    
+        if self.use_taac:
+            d3 = self.taac2(d3, d2)
+        feature_maps['dec3'] = d3
+    
+        d4 = self.up4(d3, x1)
+        if self.use_taac:
+            d4 = self.taac3(d4, d3)
+        feature_maps['dec4'] = d4
+    
+        # -------- Output --------
+        out = self.outc(torch.cat([d4, x1], dim=1))
+        feature_maps['output'] = out
+    
+        return out, feature_maps
 
 
 #Initialize teh model and send it to GPU
@@ -90,3 +98,10 @@ if torch.cuda.device_count() > 1:
     model = torch.nn.DataParallel(model)
     
 model.to(device)
+
+if torch.__version__ >= "2.0":
+    model = torch.compile(
+        model,
+        mode="reduce-overhead",
+        fullgraph=False
+    )
